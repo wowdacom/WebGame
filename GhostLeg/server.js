f74a3c7ea4d0daea44f53547bb8a39b9a2999411
Jim <wowdacom@gmail.com>
2018-05-15 18:15:21 +0800

    my game

--- GhostLeg/server.js
+++ GhostLeg/server.js
@@ -1,52 +1,27 @@
-#!/usr/bin/env node
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
-
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
-
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
-    
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-});
+var fs = require('fs');
+  // 1
+  var app = require('http').createServer(function(req, res) {
+     console.log("Websocket server is listening...")
+      fs.readFile('client.html', function(err, data) {
+          if(err) {
+              res.writeHead(500);
+              return res.end('Error reading default index.');
+          } else {
+              res.writeHead(200);
+              res.end(data);
+          }
+      });
+  });
+  // 2
+  var io = require('socket.io').listen(app);
+  io.sockets.on('connection', function(socket) {
+      // 3
+      socket.on('echopost', function(data) {
+          // 4
+          console.log("Get data");
+          var date = new Date();
+          socket.emit('echoback', '['+date+'] '+data);
+      });
+  });
+  
+app.listen(8080);
